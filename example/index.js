d7d18d32c001939fa4832eae67f7edd054af010a
Christopher Jeffrey <chjjeffrey@gmail.com>
2014-08-29 13:54:54 -0700

    improve example and lib/index.js

--- example/index.js
+++ example/index.js
@@ -2,6 +2,10 @@
 
 var bitcoind = require('../')();
 
+bitcoind.on('error', function(err) {
+  console.log('bitcoind: error="%s"', err.message);
+});
+
 bitcoind.on('open', function(status) {
-  console.log('bitcoind: ' + status);
+  console.log('bitcoind: status=%s', status);
 });
