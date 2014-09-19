4410d9b9e99b12dd81c68717590540755f3845a9
Christopher Jeffrey <chjjeffrey@gmail.com>
2014-09-19 16:45:46 -0700

    fix shutdown. sigint/sighup handlers.

--- example/index.js
+++ example/index.js
@@ -12,10 +12,15 @@ bitcoind.start(function(err) {
   bitcoind.on('open', function(status) {
     setTimeout(function() {
       var genesis = '0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f';
-      return bitcoind.getBlock(genesis, function(err, block) {
-        if (err) return console.log(err.message);
-        print(block);
-      });
+      (function next(hash) {
+        return bitcoind.getBlock(hash, function(err, block) {
+          if (err) return console.log(err.message);
+          print(block);
+          if (process.argv[2] === '-r' && block.nextblockhash) {
+            setTimeout(next.bind(null, block.nextblockhash), 200);
+          }
+        });
+      })(genesis);
     }, 1000);
     console.log('bitcoind: status="%s"', status);
   });
