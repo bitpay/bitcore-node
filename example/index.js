6eb95cf29351d6e6d7bbb2f2ecb82c79a51432f6
Christopher Jeffrey <chjjeffrey@gmail.com>
2014-09-25 15:13:59 -0700

    minor: output.

--- example/index.js
+++ example/index.js
@@ -18,11 +18,11 @@ var genesisBlock = '0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a
 var genesisTx = '0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b';
 
 bitcoind.on('error', function(err) {
-  print('bitcoind: error="%s"', err.message);
+  print('error="%s"', err.message);
 });
 
 bitcoind.on('open', function(status) {
-  print('bitcoind: status="%s"', status);
+  print('status="%s"', status);
 
   // getBlocks(bitcoind);
 
@@ -86,7 +86,10 @@ function inspect(obj) {
 
 function print(obj) {
   if (typeof obj === 'string') {
-    return process.stdout.write(util.format.apply(util, arguments) + '\n');
+    return process.stdout.write(
+      'bitcoind.js: '
+      + util.format.apply(util, arguments)
+      + '\n');
   }
   return process.stdout.write(inspect(obj) + '\n');
 }
