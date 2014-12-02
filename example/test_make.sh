34de63fb23ee44e9be76bb6953b0d2e55e4f9f6f
Christopher Jeffrey <chjjeffrey@gmail.com>
2014-12-02 03:41:51 -0800

    remove example for now.

--- example/test_make.sh
+++
@@ -1,7 +0,0 @@
-g++ -I$HOME/bitcoin/src -I$HOME/bitcoin/src/obj -I$HOME/bitcoin/src/config \
-  -I$HOME/bitcoin/src/leveldb/include \
-  -DHAVE_WORKING_BOOST_SLEEP_FOR -DHAVE_CONFIG_H -g -O2 \
-  -fexceptions -frtti -fpermissive -o test_btc test.cc \
-  -lboost_system -lboost_filesystem -lboost_program_options \
-  -lboost_thread -lboost_chrono -lssl -lcrypto \
-  ${HOME}/bitcoin/src/libbitcoind.so
