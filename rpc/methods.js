f12e8b4606abad8cbd4e022aa50ff99f20986214
Yemel Jardi <angel.jardi@gmail.com>
2015-02-19 17:10:43 -0300

    progress on rpc methods

--- rpc/methods.js
+++ rpc/methods.js
@@ -1,16 +1,53 @@
 'use strict';
 
 // Local Coded
+
+
+var initModule = function(node, serivces) {
+  // hook to events
+};
+
 // createmultisig nrequired ["key",...]
+var createMultisig = function(required, keys) {
+  // keys array or string.
+  // Keys may be addreses (return error).
+  // returns: address and hex-enconded redeem script
+};
+
 // createrawtransaction [{"txid":"id","vout":n},...] {"address":amount,...}
+var createRawTransaction = function(outpoints, outputs) {
+  // outpoints. array or outpoint
+  // outputs or (Address:amount)
+  // returns a raw transaction
+};
+
 // decoderawtransaction "hexstring"
+var decodeRawTransaction = function(transaction) {
+  // hex transaction
+  // result: txid, version, locktime, vin, vout
+};
+
 // decodescript "hex"
+var decodeScript = function(redeemScript) {
+  // hex redeemScript
+  // result: asm, type, reqSigs, addresses, p2sh
+};
+
 // getblock "hash" ( verbose )
+var decodeScript = function(redeemScript) {
+
+};
+
 // getblockhash index
+
 // getrawtransaction "txid" ( verbose )
+
 // gettxout "txid" n ( includemempool )
+
 // gettxoutsetinfo
+
 // sendrawtransaction "hexstring" ( allowhighfees )
+
 // signrawtransaction "hexstring" ( [{"txid":"id","vout":n,"scriptPubKey":"hex","redeemScript":"hex"},...] ["privatekey1",...] sighashtype )
 // validateaddress "bitcoinaddress"
 // verifymessage "bitcoinaddress" "signature" "message"
