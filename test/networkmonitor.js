71a322777136e29bf72a24969a5f2f88a42f7a9a
Manuel Araoz <manuelaraoz@gmail.com>
2015-02-24 19:22:28 -0300

    add barebones NetworkMonitor

--- test/networkmonitor.js
+++ test/networkmonitor.js
@@ -3,6 +3,7 @@
 var chai = require('chai');
 var should = chai.should();
 var sinon = require('sinon');
+var bitcore = require('bitcore');
 
 var NetworkMonitor = require('../lib/networkmonitor');
 var EventBus = require('../lib/eventbus');
@@ -10,13 +11,22 @@ var util = require('util');
 var Promise = require('bluebird');
 Promise.longStackTraces();
 
-var bus = sinon.createStubInstance(EventBus);
+var bus = new EventBus(); //sinon.createStubInstance(EventBus);
 
-describe('NetworkMonitor', function() {
+describe.only('NetworkMonitor', function() {
 
-  it.only('instantiate', function() {
+  this.timeout(10000);
+
+  it('instantiate', function() {
     var nm = new NetworkMonitor(bus);
     should.exist(nm);
+  });
+  it('start', function(cb) {
+    var nm = new NetworkMonitor(bus);
+    bus.register(bitcore.Transaction, function(tx) {
+      console.log('new tx: ', tx.id); 
+    });
+    nm.start();
   });
 
 });
