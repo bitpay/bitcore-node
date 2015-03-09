256614134a07d58a769ba2b0de9b15ec98126925
Chris Arnesen <chris.arnesen@gmail.com>
2015-03-08 22:02:03 -0700

    rpc initial commit

--- test/node.js
+++ test/node.js
@@ -7,21 +7,23 @@ var sinon = require('sinon');
 var Promise = require('bluebird');
 var EventEmitter = require('eventemitter2').EventEmitter2;
 var BitcoreNode = require('../lib/node');
+var BitcoindRpc = require('../lib/rpc');
 var EventBus = require('../lib/eventbus');
 Promise.longStackTraces();
 
 describe('BitcoreNode', function() {
 
   // mocks
-  var busMock, nmMock;
+  var busMock, nmMock, rpcMock;
   beforeEach(function() {
     busMock = new EventBus();
     nmMock = new EventEmitter();
     nmMock.start = function() {};
+    rpcMock = {};
   });
   describe('instantiates', function() {
     it('from constructor', function() {
-      var node = new BitcoreNode(busMock, nmMock);
+      var node = new BitcoreNode(busMock, nmMock, rpcMock);
       should.exist(node);
     });
 
@@ -32,17 +34,17 @@ describe('BitcoreNode', function() {
   });
 
   it('starts', function() {
-    var node = new BitcoreNode(busMock, nmMock);
+    var node = new BitcoreNode(busMock, nmMock, rpcMock);
     node.start.bind(node).should.not.throw();
   });
 
   it('broadcasts errors from network monitor', function(cb) {
-    var node = new BitcoreNode(busMock, nmMock);
+    var node = new BitcoreNode(busMock, nmMock, rpcMock);
     node.on('error', cb);
     nmMock.emit('error');
   });
   it('exposes all events from the event bus', function(cb) {
-    var node = new BitcoreNode(busMock, nmMock);
+    var node = new BitcoreNode(busMock, nmMock, rpcMock);
     node.on('foo', cb);
     busMock.emit('foo');
   });
