60c766a17f4ff5fe0182b5178eabc84d0b988b4d
Manuel Araoz <manuelaraoz@gmail.com>
2015-03-17 11:15:00 -0300

    remove snake_case name

--- api/test/app.js
+++ api/test/app.js
@@ -2,8 +2,6 @@
 
 var BitcoreHTTP = require('../lib/http');
 
-var get_app = function(nodeMock) {
+module.exports = function(nodeMock) {
   return process.env.INTEGRATION === 'true' ? BitcoreHTTP.create().app : new BitcoreHTTP(nodeMock).app;
 };
-
-module.exports = get_app;
