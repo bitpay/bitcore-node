348598747bf1c1e393518aa5d149a0d7542c277e
Braydon Fuller <braydon@bitpay.com>
2015-08-20 20:05:23 -0400

    Include more options and fallback to default config with start command

--- bin/start.js
+++ bin/start.js
@@ -1,45 +1,6 @@
 'use strict';
 
 var start = require('../lib/scaffold/start');
-var path = require('path');
+var defaultConfig = require('../lib/scaffold/default-config');
 
-start({
-  path: process.cwd(),
-  config: {
-    datadir: process.env.BITCORENODE_DIR || path.resolve(process.env.HOME, '.bitcoin'),
-    network: process.env.BITCORENODE_NETWORK || 'livenet',
-    port: process.env.BITCORENODE_PORT || 3001
-  }
-});
-
-node.on('stopping', function() {
-  clearInterval(interval);
-});
-
-function exitHandler(options, err) {
-  if (err) {
-    log.error('uncaught exception:', err);
-    if(err.stack) {
-      console.log(err.stack);
-    }
-    process.exit(-1);
-  }
-  if (options.sigint) {
-    node.stop(function(err) {
-      if(err) {
-        log.error('Failed to stop services: ' + err);
-        return process.exit(1);
-      }
-
-      log.info('Halted');
-      process.exit(0);
-    });
-  }
-}
-
-//catches uncaught exceptions
-
-
-process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
-//catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {sigint:true}));
+start(defaultConfig());
--- cli/bitcore-node.js
+++ cli/bitcore-node.js
@@ -4,28 +4,34 @@
 
 var program = require('commander');
 var version = require(__dirname + '/../package.json').version;
+var bitcore = require('bitcore');
+var $ = bitcore.util.preconditions;
+var path = require('path');
 var create = require('../lib/scaffold/create');
 var add = require('../lib/scaffold/add');
 var start = require('../lib/scaffold/start');
 var findConfig = require('../lib/scaffold/find-config');
+var defaultConfig = require('../lib/scaffold/default-config');
 
 program
-  .version(version)
-  .option('-d, --datadir', 'Database and configuration directory')
-  .option('-t, --testnet', 'Enable testnet network');
+  .version(version);
 
 program
   .command('create <directory> [name]')
   .description('Create a new node')
-  .action(function(dirname, name){
-    var options = {
+  .option('-d, --datadir <dir>', 'Specify the bitcoin database directory')
+  .action(function(dirname, name, cmd){
+    if (cmd.datadir) {
+      cmd.datadir = path.resolve(process.cwd(), cmd.datadir);
+    }
+    var opts = {
       cwd: process.cwd(),
       dirname: dirname,
       name: name,
-      datadir: './data',
+      datadir: cmd.datadir || './data',
       isGlobal: false
     };
-    create(options, function(err) {
+    create(opts, function(err) {
       if (err) {
         throw err;
       }
@@ -36,13 +42,16 @@ program
 program
   .command('start')
   .description('Start the current node')
-  .action(function(){
-    var configInfo = findConfig(process.cwd());
-    if (configInfo) {
-      start(configInfo);
-    } else {
-      throw new Error('Can not find bitcore-node.json in current path');
+  .option('-c, --config <dir>', 'Specify the directory with Bitcore Node configuration')
+  .action(function(cmd){
+    if (cmd.config) {
+      cmd.config = path.resolve(process.cwd(), cmd.config);
     }
+    var configInfo = findConfig(cmd.config || process.cwd());
+    if (!configInfo) {
+      configInfo = defaultConfig();
+    }
+    start(configInfo);
   });
 
 program
@@ -51,6 +60,9 @@ program
   .description('Install a module for the current node')
   .action(function(module){
     var config = findConfig();
+    if (!config) {
+      throw new Error('Could not find configuration, see `bitcore-node create --help`');
+    }
     add(config, module);
     console.log('Successfully added module: ', module);
     console.log(module);
