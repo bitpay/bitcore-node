81b2d2f2a33546a1a832368c35e1a63cdd64e2b6
Braydon Fuller <braydon@bitpay.com>
2015-08-25 13:57:54 -0400

    Added tests for start command.

--- test/scaffold/start.integration.js
+++ test/scaffold/start.integration.js
@@ -2,27 +2,38 @@
 
 var should = require('chai').should();
 var sinon = require('sinon');
+var proxyquire = require('proxyquire');
+var AddressModule = require('../../lib/modules/address');
 
 describe('#start', function() {
 
-  before(function() {
-    // setup testing directories
-  });
-
-  after(function() {
-    // cleanup testing directories
-  });
-  
   describe('will dynamically create a node from a configuration', function() {
 
-    it('require each bitcore-node module', function() {
+    it('require each bitcore-node module', function(done) {
+      var node;
+      var TestNode = function(options) {
+        options.db.modules.should.deep.equal([AddressModule]);
+      };
+      TestNode.prototype.on = sinon.stub();
+      TestNode.prototype.chain = {
+        on: sinon.stub()
+      };
 
+      var starttest = proxyquire('../../lib/scaffold/start', {
+        '../node': TestNode
+      });
+
+      node = starttest({
+        path: __dirname,
+        config: {
+          modules: [
+            'address'
+          ],
+          datadir: './data'
+        }
+      });
+      node.should.be.instanceof(TestNode);
+      done();
     });
-
-    it('create an instance of node with modules enabled', function() {
-
-    });
-
   });
-
 });
