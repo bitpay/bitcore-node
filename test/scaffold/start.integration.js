895e46dcac15c279940c0f0943441dc099179e7c
Patrick Nagurny <patrick@bitpay.com>
2015-08-31 13:37:11 -0400

    pass config to services

--- test/scaffold/start.integration.js
+++ test/scaffold/start.integration.js
@@ -9,13 +9,14 @@ describe('#start', function() {
 
   describe('will dynamically create a node from a configuration', function() {
 
-    it('require each bitcore-node service', function(done) {
+    it('require each bitcore-node service with default config', function(done) {
       var node;
       var TestNode = function(options) {
         options.services[0].should.deep.equal({
           name: 'address',
           module: AddressService,
-          dependencies: ['bitcoind', 'db']
+          dependencies: ['bitcoind', 'db'],
+          config: {}
         });
       };
       TestNode.prototype.on = sinon.stub();
@@ -32,6 +33,45 @@ describe('#start', function() {
         config: {
           services: [
             'address'
+          ],
+          datadir: './data'
+        }
+      });
+      node.should.be.instanceof(TestNode);
+      done();
+    });
+
+    it('require each bitcore-node service with explicit config', function(done) {
+      var node;
+      var TestNode = function(options) {
+        options.services[0].should.deep.equal({
+          name: 'address',
+          module: AddressService,
+          dependencies: ['bitcoind', 'db'],
+          config: {
+            param: 'test'
+          }
+        });
+      };
+      TestNode.prototype.on = sinon.stub();
+      TestNode.prototype.chain = {
+        on: sinon.stub()
+      };
+
+      var starttest = proxyquire('../../lib/scaffold/start', {
+        '../node': TestNode
+      });
+
+      node = starttest({
+        path: __dirname,
+        config: {
+          services: [
+            {
+              name: 'address',
+              config: {
+                param: 'test'
+              }
+            }
           ],
           datadir: './data'
         }
