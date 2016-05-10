0387c1a6e41a21d4cc9774846ddd415443c73de9
Braydon Fuller <braydon@bitpay.com>
2016-05-10 11:47:31 -0400

    test: increase timeout for cluster test and decrease keypool
    resolves issues when the keypool takes time to fill

--- regtest/cluster.js
+++ regtest/cluster.js
@@ -52,7 +52,7 @@ describe('Bitcoin Cluster', function() {
 
   before(function(done) {
     log.info('Starting 3 bitcoind daemons');
-    this.timeout(20000);
+    this.timeout(60000);
     async.each(nodesConf, function(nodeConf, next) {
       var opts = [
         '--regtest',
