58223948acca952c28ba62a5b435ab656e44fe4a
Chris Kleeschulte <chrisk@bitpay.com>
2017-05-30 15:42:01 -0400

    wip

--- regtest/test_bus.js
+++ regtest/test_bus.js
@@ -26,16 +26,51 @@ TestBusService.prototype.start = function(callback) {
   self.bus = self.node.openBus({ remoteAddress: 'localhost' });
 
   self.bus.on('p2p/transaction', function(tx) {
-    self.pubSocket.send([ 'transaction', new Buffer(tx.uncheckedSerialize(), 'hex') ]);
+    var self = this;
+    self._cache.transaction.push(tx);
+    if (self._ready) {
+      for(var i = 0; i < self._cache.transaction.length; i++) {
+        var transaction = self._cache.transaction.unshift();
+        self.pubSocket.send([ 'transaction', new Buffer(transaction.uncheckedSerialize(), 'hex') ]);
+      }
+      return;
+    }
   });
 
   self.bus.on('p2p/block', function(block) {
-    self.pubSocket.send([ 'block', block.toBuffer() ]);
+    var self = this;
+    self._cache.block.push(block);
+    if (self._ready) {
+      for(var i = 0; i < self._cache.block.length; i++) {
+        var blk = self._cache.block.unshift();
+        self.pubSocket.send([ 'block', blk.toBuffer() ]);
+      }
+      return;
+    }
+  });
+
+  self.bus.on('p2p/headers', function(headers) {
+    var self = this;
+    self._cache.headers.push(headers);
+    if (self._ready) {
+      for(var i = 0; i < self._cache.headers.length; i++) {
+        var hdrs = self._cache.headers.unshift();
+        self.pubSocket.send([ 'headers', hdrs ]);
+      }
+      return;
+    }
   });
 
   self.bus.subscribe('p2p/transaction');
   self.bus.subscribe('p2p/block');
 
+  self.node.on('ready', function() {
+    //we could be getting events right away, so until our subscribers get connected, we need to cache
+    self._cache = { transaction: [], block: [], headers: [] };
+    setTimeout(function() {
+      self._ready = true;
+    }, 2000);
+  });
   callback();
 };
 
