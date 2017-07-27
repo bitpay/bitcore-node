2bd769cce026231b421302570f11d4868174f722
Chris Kleeschulte <chrisk@bitpay.com>
2017-07-27 19:12:23 -0400

    wip

--- test/services/db/index.unit.js
+++ test/services/db/index.unit.js
@@ -1,56 +1,154 @@
 'use strict';
 
-var expect = require('chai').expect;
-var bitcore = require('bitcore-lib');
-var DB = require('../../../lib/services/db');
+var chai = require('chai');
+var should = chai.should();
+var assert = chai.assert;
+var expect = chai.expect;
+var DBService = require('../../../lib/services/db');
+var sinon = require('sinon');
+var Levelup = require('levelup');
 
 describe('DB', function() {
 
-  describe('Reorg', function() {
+  var dbService;
 
-    before(function() {
-      this.db = new DB({
-        node: {
-          network: bitcore.Networks.testnet,
-          datadir: '/tmp',
-          services: ''
-        }
+  var sandbox;
+  beforeEach(function() {
+    sandbox = sinon.sandbox.create();
+    dbService = new DBService({
+      node: {
+        services: [],
+        datadir: '/tmp',
+        network: 'regtest',
+        on: sinon.stub()
+      }
+    });
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
+  describe('#start', function() {
+    it('should start the db service by creating a db dir, ' +
+      ' if necessary, and setting the store', function(done) {
+      dbService.start(function() {
+        dbService._store.should.be.instanceOf(Levelup);
+        done();
       });
-      this.db.tip = { hash: 'ff', height: 444 };
+    });
+  });
+
+  describe('#stop', function() {
+    it('should stop if store not open', function(done) {
+      dbService.stop(function() {
+        var close = sandbox.stub().callsArg(0);
+        dbService._store = { close: close };
+        dbService._stopping.should.be.true;
+        done();
+      });
     });
 
-    it('should detect a reorg from a common ancenstor that is in our set', function() {
-      var block1 = { hash: '11', header: { prevHash: new Buffer('ff', 'hex') } };
-      var block2 = { hash: '22', header: { prevHash: new Buffer('11', 'hex') } };
-      var block3 = { hash: '33', header: { prevHash: new Buffer('22', 'hex') } };
-      var block4 = { hash: '44', header: { prevHash: new Buffer('22', 'hex') } };
-      //blocks must be passed in the order that they are received.
-      var blocks = [ block3, block2, block1, block4 ];
-      expect(this.db.detectReorg(blocks)).to.deep.equal(block3);
+    it('should stop if store open', function(done) {
+      dbService.stop(function() {
+        var close = sandbox.stub().callsArg(0);
+        dbService._store = { close: close, isOpen: sinon.stub().returns(true) };
+        dbService._stopping.should.be.true;
+        done();
+      });
+    });
+  });
 
+  describe('#_onError', function() {
+    it('should stop the db', function() {
+      var stop = sandbox.stub();
+      dbService.node = { stop: stop };
+      dbService._onError(new Error('some error'));
+      stop.should.be.calledOnce;
+    });
+  });
+
+  describe('#_setDataPath', function() {
+
+    it('should set the data path', function() {
+      dbService._setDataPath();
+      dbService.dataPath.should.equal('/tmp/regtest/bitcore-node.db');
     });
 
-    it('should detect a reorg from a common ancenstor that is not in our set', function() {
-      var block1 = { hash: '11', header: { prevHash: new Buffer('ff', 'hex') } };
-      var block2 = { hash: '22', header: { prevHash: new Buffer('11', 'hex') } };
-      var block3 = { hash: '33', header: { prevHash: new Buffer('22', 'hex') } };
-      var block4 = { hash: '44', header: { prevHash: new Buffer('ee', 'hex') } };
-      var blocks = [ block3, block2, block1, block4 ];
-      expect(this.db.detectReorg(blocks)).to.deep.equal(block4);
+  });
 
+  describe('#_setVersion', function() {
+    it('should set the version', function(done) {
+      var put = sandbox.stub(dbService, 'put').callsArgWith(2, null);
+      dbService._setVersion(function(err) {
+        put.should.be.calledOnce;
+        put.args[0][0].toString('hex').should.deep.equal('ffff76657273696f6e');
+        put.args[0][1].toString('hex').should.deep.equal('00000001');
+        done();
+      });
+    });
+  });
+
+  describe('#get', function() {
+    it('should get a value from the db', function(done) {
+      var get = sandbox.stub().callsArgWith(2, null, 'data');
+      dbService._store = { get: get };
+      dbService.get('key', function(err, value) {
+        if (err) {
+          return done(err);
+        }
+        value.should.equal('data');
+        done();
+      });
     });
 
-    it('should not detect a reorg', function() {
-      this.db.reorgTipHash = null;
-      var block1 = { hash: '11', header: { prevHash: new Buffer('ff', 'hex') } };
-      var block2 = { hash: '22', header: { prevHash: new Buffer('11', 'hex') } };
-      var block3 = { hash: '33', header: { prevHash: new Buffer('22', 'hex') } };
-      var block4 = { hash: '44', header: { prevHash: new Buffer('33', 'hex') } };
-      var blocks = [ block3, block2, block1, block4 ];
-      var actual = this.db.detectReorg(blocks);
-      expect(actual).to.be.undefined;
+    it('should not get a value while the node is shutting down', function(done) {
+      dbService._stopping = true;
+      dbService.get('key', function(err, value) {
+        err.message.should.equal('Shutdown sequence underway, not able to complete the query');
+        done();
+      });
     });
 
+  });
+
+  describe('#put', function() {
+    it('should put a value in the db', function(done) {
+      var put = sandbox.stub().callsArgWith(2, null);
+      dbService._store = { put: put };
+      dbService.put(new Buffer('key'), new Buffer('value'), function(err) {
+        if (err) {
+          return done(err);
+        }
+        put.should.be.calledOnce;
+        done();
+      });
+    });
+
+    it('should not allow an operation while the node is shutting down', function(done) {
+      dbService._stopping = true;
+      dbService.put(new Buffer('key'), new Buffer('value'), function(err) {
+        done();
+      });
+    });
+  });
+
+  describe('#batch', function() {
+  });
+
+  describe('#createReadStream', function() {
+  });
+
+  describe('#createKeyStream', function() {
+  });
+
+  describe('#close', function() {
+  });
+
+  describe('#getServiceTip', function() {
+  });
+
+  describe('#getPrefix', function() {
   });
 });
 
