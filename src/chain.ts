d2d4f23d6e24896e502ac53b836249f6f6d513f2
Omar Sagga <osagga@bu.edu>
2018-07-25 11:10:24 -0400

    Add LTC support
    Signed-off-by: Omar Sagga <osagga@bu.edu>

--- src/chain.ts
+++ src/chain.ts
@@ -7,4 +7,8 @@ module.exports = {
     lib: require('bitcore-lib-cash'),
     p2p: require('bitcore-p2p-cash'),
   },
+  LTC: {
+    lib: require('litecore-lib'),
+    p2p: require('litecore-p2p'),
+    }
 }
